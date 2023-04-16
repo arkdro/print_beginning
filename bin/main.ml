0c67f69acdb1c8ce91d4489be88b7bd16ab45378
Arkadii Drovosekov <arkdro@protonmail.com>
2023-04-16 19:28:44 +0200

    init

---
+++ bin/main.ml
@@ -0,0 +1,5 @@
+(* open Files *)
+
+let () =
+  (* Files.read_lines "/tmp/f1.txt" *)
+  print_endline "\nHello, World!"
---
+++ lib/read_lines.ml
@@ -0,0 +1,13 @@
+let read_one_line fd =
+  input_line fd
+
+let read_lines filename =
+  let f = open_in filename in
+  let rec loop () =
+    try
+      let next = input_line f in
+      next :: loop ()
+    with End_of_file ->
+      close_in f;
+      []
+  in loop ()
