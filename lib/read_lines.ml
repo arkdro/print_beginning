85208d5b22e423058859dc2e30416fa1c7388d50
Arkadii Drovosekov <arkdro@protonmail.com>
2023-04-16 19:28:45 +0200

    reformat

--- lib/read_lines.ml
+++ lib/read_lines.ml
@@ -1,5 +1,4 @@
-let read_one_line fd =
-  input_line fd
+let read_one_line fd = input_line fd
 
 let read_lines filename =
   let f = open_in filename in
@@ -10,4 +9,5 @@ let read_lines filename =
     with End_of_file ->
       close_in f;
       []
-  in loop ()
+  in
+  loop ()
