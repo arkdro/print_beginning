93f980755ec158a899be6869a4d30812e8ff44a1
Arkadii Drovosekov <arkdro@protonmail.com>
2023-04-16 19:28:45 +0200

    change output header

--- bin/main.ml
+++ bin/main.ml
@@ -3,5 +3,5 @@ open Files
 let () =
   let lines = Line_processor.read_lines "/tmp/f1.txt" in
   let line = String.concat "\n" lines in
-  print_endline "\nHello, World!";
+  print_endline "The lines:";
   print_endline line
