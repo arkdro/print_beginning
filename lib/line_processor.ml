de4b83aeca80790ac1ed62d77a9cfcdd36b38d94
Arkadii Drovosekov <arkdro@protonmail.com>
2023-04-16 19:28:45 +0200

    reformat

--- lib/line_processor.ml
+++ lib/line_processor.ml
@@ -1,5 +1,4 @@
 let default_max_lines = 5
-
 let read_one_line fd = input_line fd
 
 let read_lines filename max_lines =
@@ -7,15 +6,15 @@ let read_lines filename max_lines =
   let rec loop counter max_lines =
     match counter with
     | n when n = max_lines ->
-       close_in f;
-       []
-    | _ ->
-       try
-         let next = input_line f in
-         next :: loop (counter+1) max_lines
-       with End_of_file ->
-         close_in f;
-         []
+        close_in f;
+        []
+    | _ -> (
+        try
+          let next = input_line f in
+          next :: loop (counter + 1) max_lines
+        with End_of_file ->
+          close_in f;
+          [])
   in
   loop 0 max_lines
 
