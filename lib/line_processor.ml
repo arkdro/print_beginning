d439a12442e5c679423601fd1e68f8fe6c376add
Arkadii Drovosekov <arkdro@protonmail.com>
2023-04-16 19:28:45 +0200

    add default numbers

--- lib/line_processor.ml
+++ lib/line_processor.ml
@@ -1,13 +1,18 @@
+let default_max_lines = 5
+
 let read_one_line fd = input_line fd
 
-let read_lines filename =
+let read_lines filename max_lines =
   let f = open_in filename in
-  let rec loop () =
+  let rec loop counter max_lines =
     try
       let next = input_line f in
-      next :: loop ()
+      next :: loop (counter+1) max_lines
     with End_of_file ->
       close_in f;
       []
   in
-  loop ()
+  loop 0 max_lines
+
+let read_lines filename =
+  read_lines filename default_max_lines
