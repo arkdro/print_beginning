615457048f7bcca3f1017a4d64bfbeeff235cdeb
Arkadii Drovosekov <arkdro@protonmail.com>
2023-04-16 19:28:45 +0200

    change default regex

--- lib/defaults.ml
+++ lib/defaults.ml
@@ -1,5 +1,5 @@
 let default_directory = "."
 let default_max_lines = 5
 let default_n_lines_arg = 5
-let default_file_match = {|[0-9]*\.txt.*|}
+let default_file_match = {|[0-9].*\.txt.*|}
 let default_compact = false
