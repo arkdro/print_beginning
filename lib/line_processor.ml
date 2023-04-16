b76787f21caad54f4fa65a8d3bc03e5f6ca45c8e
Arkadii Drovosekov <arkdro@protonmail.com>
2023-04-16 19:28:45 +0200

    check counter

--- lib/line_processor.ml
+++ lib/line_processor.ml
@@ -5,12 +5,17 @@ let read_one_line fd = input_line fd
 let read_lines filename max_lines =
   let f = open_in filename in
   let rec loop counter max_lines =
-    try
-      let next = input_line f in
-      next :: loop (counter+1) max_lines
-    with End_of_file ->
-      close_in f;
-      []
+    match counter with
+    | n when n = max_lines ->
+       close_in f;
+       []
+    | _ ->
+       try
+         let next = input_line f in
+         next :: loop (counter+1) max_lines
+       with End_of_file ->
+         close_in f;
+         []
   in
   loop 0 max_lines
 
