fbc4f11dda9a7502fe0b712866f2c6e9016c4b79
Arkadii Drovosekov <arkdro@protonmail.com>
2023-04-16 19:28:45 +0200

    remove debug print

--- bin/main.ml
+++ bin/main.ml
@@ -3,7 +3,4 @@ open Files
 let () =
   let (directory, n_lines, file_match, compact) = Args.get_args () in
   let _ = Dir_processor.process_dir directory n_lines file_match compact in
-  print_endline "The args:";
-  print_endline directory;
-  print_endline (Printf.sprintf "%i" n_lines);
-  print_endline (Printf.sprintf "%b" compact);
+  ()
